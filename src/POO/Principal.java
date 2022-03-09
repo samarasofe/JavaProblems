ebcafd47bcec21ef7af50ebe806d6f85c8977168
Samara Ferreira <samara.sofe@gmail.com>
2022-03-09 14:19:44 -0300

    IMC CALC

---
+++ src/POO/Principal.java
@@ -0,0 +1,24 @@
+package POO;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		System.out.println("Por favor digite o seu peso (kg):");
+		Scanner teclado = new Scanner(System.in);
+ 		
+		float peso = teclado.nextFloat();
+		
+		System.out.println("Digite sua altura (m): ");
+		
+		float altura = teclado.nextFloat();
+		
+		float imc = peso / (altura*altura);
+		
+		System.out.printf("Seu IMC eh: %.2f " , imc);
+
+		
+	}
+
+}
